4b771e9d30cc5e58e2b66281719edb12d68769d3
WrongNicholas <mnwojnar2000@gmail.com>
2025-12-26 03:14:25 -0500

    Undo repo swap

--- src/main/java/com/example/helloworld/HelloWorldPlugin.java
+++ src/main/java/com/example/helloworld/HelloWorldPlugin.java
@@ -2,20 +2,35 @@ package com.example.helloworld;
 
 import net.woji.api.Plugin;
 import net.woji.api.PluginContext;
+import net.woji.api.event.ServerTickEvent;
 import org.slf4j.Logger;
 
 public class HelloWorldPlugin implements Plugin {
-  PluginContext pluginContext;
+  private PluginContext pluginContext;
+  private Logger logger;
 
   public void init(PluginContext pluginContext)
   {
     this.pluginContext = pluginContext;
+    this.logger = pluginContext.getLogger();
+
+    pluginContext.getEventBus().register(this);
   }
 
   public void onLoad() {
-    Logger log = pluginContext.getLogger();
-    log.info("Hello from HelloWorldPlugin!");
+
   }
-  public void onEnable() {}
-  public void onDisable() {}
+
+  public void onEnable() {
+    logger.info("HelloWorldPlugin enabled!");
+  }
+  public void onDisable() {
+    logger.info("HelloWorldPlugin disabled!");
+  }
+
+  public void onTick(ServerTickEvent e) {
+    if (e.getTick() % 100 == 0) {
+      logger.info("Hello from HelloWorldPlugin!");
+    }
+  }
 }
